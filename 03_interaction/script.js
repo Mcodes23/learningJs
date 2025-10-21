640c382ab940f436ab51905a23973580f25e0aa3
Mcodes23 <mateliboniface@gmail.com>
2025-10-21 20:34:13 +0300

    Feat: user interaction functions

---
+++ 03_interaction/script.js
@@ -0,0 +1,31 @@
+"use strict";
+
+/**
+ * * Javascript Interaction
+ * -----------------------
+ * we can Interact with the user using these functions:
+ * * 1. alert()
+ * It shows a message and waits for the user to press “OK”.
+ * * Example
+ * alert("Hello");
+ * 
+ * * 2. prompt()
+ * The function prompt accepts two arguments
+ * * Syntax
+ * result = prompt(title, [default]);
+ * It shows a modal window with a text message, an input field for the visitor, and the buttons OK/Cancel.
+ * * Example
+ * let userName = prompt("Enter your name: ", name);
+ * alert(`Your name is ${userName}`)
+ * 
+ * * 3. confirm()
+ * The function confirm shows a modal window with a question and two buttons: OK and Cancel.
+ * * Syntax
+ * result = confirm(question);
+ * 
+ * The result is true if OK is pressed and false otherwise.
+ * * Example
+ * let isBoss = confirm("Are you the boss?");
+ * alert( isBoss );
+ */
+ 
