874db2c8483c9faf0ef642249ae864953c19b343
Mcodes23 <mateliboniface@gmail.com>
2025-10-24 16:06:55 +0300

    comparisons

---
+++ 06_comparisons/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/**
+ * * Comparisons
+ * They include:
+ * Greater/less than: a > b, a < b
+ * Greater/less than or equals: a >= b, a <= b.
+ * Equals: a == b, please note the double equality sign == means the equality test, while a single one a = b means an assignment.
+ * Not equals: it’s written as a != b.
+ * 
+ * * All comparison operators return a boolean value:
+ * true – means “yes”, “correct” or “the truth”.
+ * false – means “no”, “wrong” or “not the truth”.
+ * 
+ * * String comparison
+ * JavaScript uses the so-called “dictionary” or “lexicographical” order.
+ * strings are compared letter-by-letter.
+ * 
+ * * Strict equality
+ * A strict equality operator === checks the equality without type conversion.
+ * There is also a “strict non-equality” operator !== analogous to !=.
+ */
