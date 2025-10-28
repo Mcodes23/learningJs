3d82ceadb109a978435443fac5f9d3692c47ab08
Mcodes23 <mateliboniface@gmail.com>
2025-10-28 09:45:22 +0300

    nullish coalescing operator

--- 08_logical-operators/script.js
+++ 08_logical-operators/script.js
@@ -58,4 +58,31 @@
  * * Example
  * console.log( !true ); // false
  * console.log( !0 ); // true
+ * 
+ * * ??(Nullish Coalescing)
+ * is a logical operator that returns its right-hand side operand when its left-hand side operand is null or undefined, and otherwise returns its left-hand side operand.
+ * 
+ * * syntax
+ * leftExpr ?? rightExpr
+ * 
+ * * Example
+ * const nullValue = null;
+ * const emptyText = ""; // falsy
+ * const someNumber = 42;
+ * 
+ * const valA = nullValue ?? "default for A";
+ * const valB = emptyText ?? "default for B";
+ * const valC = someNumber ?? 0;
+ * 
+ * console.log(valA); // "default for A"
+ * console.log(valB); // "" (as the empty string is not null or undefined)
+ * console.log(valC); // 42
+ * 
+ * The common use case for ?? is to provide a default value.
+ * 
+ * * Comparison wit ||
+ * || returns the first truthy value.
+ * ?? returns the first defined value.
+ * 
+ * ! It’s forbidden to use it with || or && without explicit parentheses.
  */
