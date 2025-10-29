a691bafa5b986b724404314a9c3a1824dbce3b3b
Mcodes23 <mateliboniface@gmail.com>
2025-10-29 10:03:45 +0300

    switch statement

---
+++ 10_switch-statement/script.js
@@ -0,0 +1,41 @@
+"use strict";
+
+/**
+ * * The "switch" statement
+ * A switch statement can replace multiple if checks.
+ * It gives a more descriptive way to compare a value with multiple variants.
+ *
+ * * Syntax
+ * switch(x) {
+ * case 'value1':  // if (x === 'value1')
+ *  ...
+ *  [break]
+ *
+ * case 'value2':  // if (x === 'value2')
+ *  ...
+ *  [break]
+ *
+ * default: // no case matched
+ *  ...
+ *  [break]
+ * }
+ *
+ * * Example
+ *
+ * let a = 2 + 2;
+ *
+ * switch (a) {
+ *  case 3:
+ *      alert( 'Too small' );
+ *      break;
+ *  case 4:
+ *      alert( 'Exactly!' );
+ *      break;
+ * case 5:
+ *      alert( 'Too big' );
+ *      break;
+ * default:
+ *      alert( "I don't know such values" );
+ * }
+ * 
+ */
