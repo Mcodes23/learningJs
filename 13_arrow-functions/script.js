60564418c5ac7656043eaa4341fa8ebcbaf2edd0
Mcodes23 <mateliboniface@gmail.com>
2025-10-29 14:51:39 +0300

    arrow functions

---
+++ 13_arrow-functions/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+/**
+ * * Arrow functions
+ * it's a simple and concise syntax for creating functions.
+ * 
+ * * Syntax
+ * let func = (arg1, arg2, ..., argN) => expression;
+ * 
+ * * Example
+ * let sum = (a, b) => a + b;
+ * 
+ * console.log( sum(1, 2) );
+ * 
+ * If we have only one argument, then parentheses around parameters can be omitted, making that even shorter.
+ * If there are no arguments, parentheses are empty, but they must be present
+ * * Example
+ * let sayHi = () => alert("Hello!");
+ * 
+ * sayHi();
+ * 
+ */
+
