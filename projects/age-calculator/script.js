d56a53ce38a35301f2cdee3e2ff717a132aba607
Mcodes23 <mateliboniface@gmail.com>
2025-10-30 10:27:10 +0300

    feat: loops and validation

--- projects/age-calculator/script.js
+++ projects/age-calculator/script.js
@@ -1,22 +1,35 @@
 "use strict";
 
-let birthYearInput = prompt("Enter your birth year: ", 0);
-let currentYearInput = prompt("Enter current year: ", 0);
+let checkYear = true;
 
-if (birthYearInput === null || currentYearInput === null) {
-  alert("You must enter both years!");
-} else {
+while (checkYear) {
+  let birthYearInput = prompt("Enter your birth year:");
+  let currentYearInput = prompt("Enter current year:");
+
+  if (birthYearInput === null || currentYearInput === null) {
+    alert("You must enter both years!");
+    continue;
+  }
+
   let birthYear = Number(birthYearInput);
   let currentYear = Number(currentYearInput);
 
   if (isNaN(birthYear) || isNaN(currentYear)) {
     alert("Please enter valid numbers for years.");
-  } else if (birthYear <= 0 || currentYear <= 0) {
-    alert("Year cannot be zero or negative.");
-  } else if (currentYear < birthYear) {
-    alert("Current year cannot be less than birth year.");
-  } else {
-    let age = currentYear - birthYear;
-    alert(`You are ${age} years old`);
+    continue;
   }
+
+  if (birthYear <= 0 || currentYear <= 0) {
+    alert("Year cannot be zero or negative.");
+    continue;
+  }
+
+  if (currentYear < birthYear) {
+    alert("Current year cannot be less than birth year.");
+    continue;
+  }
+
+  let age = currentYear - birthYear;
+  alert(`You are ${age} years old.`);
+  checkYear = false;
 }
